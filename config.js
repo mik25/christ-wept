61f4bd892083555f634f9767d37dbb8b828cfdbb
mik25 <mikmc55@gmail.com>
2023-03-07 19:10:08 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'ℋ HIMYM 󠁴󠁭󠁢󠁿  Ỵ', // unique addon name
+  prefix: 'HIMYM', // unique addon prefix (must be different for all addons!)
+  icon: 'https://christwept.surge.sh/logo.png', // set the correct domain name here so the logo is accessible
+  description: 'Some telly thing', // addon description
+  connLimit: false, // can be false or an integer, if set to an integer, it will show a message with "Limited to X connections."
+  paginate: 100, // how many catalogs to show per page
+  playlistUrl: 'http://webo.asia:80/get.php?username=newstalker&password=local&type=m3u_plus', // the URL to the M3U playlist
+}
--- index.js
+++ index.js
@@ -1,48 +1,133 @@
-// Importing required modules
-const addonBuilder = require('stremio-addon-sdk').addonBuilder;
-const http = require('http');
-const fetch = require('node-fetch');
+const m3u = require('m3u8-reader')
+const needle = require('needle')
+const async = require('async')
+const base64 = require('base-64')
 
-// Creating a new addonBuilder instance
+const m3uArray = [
+	'./playlists/playlist.m3u',
+]
+
+let idx = 0
+
+const groupItems = {
+
+}
+
+const defaults = require('./config.js')
+
+if (defaults.connLimit)
+	defaults.description += ' Limited to ' + defaults.connLimit + ' connections.'
+
+
+m3uArray.forEach(el => {
+	let data = require('fs').readFileSync(require('path').resolve(el))
+	data = Buffer.isBuffer(data) ? data.toString() : data
+	let lastName = false
+	let lastGroup = false
+	data.split('\r\n').forEach(line => {
+		if (lastName) {
+			if (lastGroup && line.startsWith('http')) {
+				if (!groupItems[lastGroup]) {
+					groupItems[lastGroup] = { id: defaults.prefix + idx, name: lastGroup, items: [] }
+					idx++
+				}
+				groupItems[lastGroup].items.push({ title: lastName, url: line })
+			}
+			lastName = false
+		}
+		if (line.includes('group-title="')) {
+			let matches = line.match(/group-title=\"[^\"]+\"/gm)
+			if ((matches || []).length) {
+				const groupName = matches[0].replace('group-title="', '').replace('"', '')
+				const nameParts = line.split(',')
+				lastName = nameParts[nameParts.length -1]
+				lastGroup = groupName
+			}
+		}
+	})
+})
+
+const catalogs = []
+
+const { addonBuilder } = require('stremio-addon-sdk')
+
+if (!catalogs.length)
+	catalogs.push({
+		id: defaults.prefix + 'cat',
+		name: defaults.name,
+		type: 'tv',
+//		extra: [{ name: 'search' }]
+	})
+
+const types = ['tv']
+
+types.push('channel')
+
 const builder = new addonBuilder({
-    id: 'org.himymaddon',
-    version: '1.0.0',
-    name: 'HIMYM',
-    catalogs: [],
-    resources: ['stream'],
-    types: ['series'],
-    idPrefixes: ['tt']
-});
+	id: 'org.' + defaults.name.toLowerCase().replace(/[^a-z]+/g,''),
+	version: '1.0.1',
+	name: defaults.name,
+	description: defaults.description,
+	resources: ['stream', 'meta', 'catalog'],
+	types,
+	idPrefixes: [defaults.prefix],
+	icon: defaults.icon,
+	catalogs
+})
 
-// Defining the stream handler
-builder.defineStreamHandler(({ type, id }) => {
-    if (type === 'series' && id === 'tt0460649') {
-        return fetch('https://raw.githubusercontent.com/mik25/christ-wept/main/shite.m3u')
-            .then(response => response.text())
-            .then(data => {
-                const streams = data.split('\n')
-                    .filter(line => line.startsWith('http'))
-                    .map(url => ({ url }));
-                return { streams };
-            });
-    } else {
-        return Promise.resolve({ streams: [] });
-    }
-});
+builder.defineCatalogHandler(args => {
+	const metas = []
+	for (let key in groupItems) {
+		metas.push({
+			name: groupItems[key].name,
+			id: groupItems[key].id,
+			type: 'channel',
+			posterShape: 'landscape'
+		})
+	}
+	return Promise.resolve({ metas })
+})
 
-// Creating the http server
-const server = http.createServer((req, res) => {
-    // Serving the addon interface
-    if (req.url === '/') {
-        res.setHeader('content-type', 'text/html');
-        res.end(builder.getInterface());
-    }
-});
+builder.defineMetaHandler(args => {
+	return new Promise((resolve, reject) => {
+		let thisGroup
+		for (let key in groupItems) {
+			if (groupItems[key].id == args.id) {
+				thisGroup = groupItems[key]
+			}
+		}
+		const meta = {
+			name: thisGroup.name,
+			id: thisGroup.id,
+			type: 'channel',
+			posterShape: 'landscape',
+		}
+		if (defaults.connLimit)
+			meta.description = 'Limited to ' + defaults.connLimit + ' connections.'
+		const i = args.id.replace(defaults.prefix, '')
+		meta.videos = thisGroup.items.map((el, ij) => {
+			return {
+				id: defaults.prefix + 'stream_' + i + '_' + ij,
+				title: el.title
+			}
+		})
+		resolve({ meta })
+	})
+})
 
-// Starting the server
-const port = process.env.PORT || 7000;
-server.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+builder.defineStreamHandler(args => {
+	return new Promise(async (resolve, reject) => {
+		const parts = args.id.split(defaults.prefix + 'stream_')[1]
+		const groupId = parts.split('_')[0]
+		const urlId = parts.split('_')[1]
+		let thisGroup
+		for (let key in groupItems) {
+			if (groupItems[key].id == defaults.prefix + groupId) {
+				thisGroup = groupItems[key]
+			}
+		}
+		resolve({ streams: [thisGroup.items[urlId]] })
+	})
+})
 
-
+module.exports = builder.getInterface()
